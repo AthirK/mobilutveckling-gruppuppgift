b170e7f8c90c79c09399331c5a70eacbfdee51e5
Johanna Lundby <johanna.lundby@gmail.com>
2025-10-06 11:47:26 +0200

    feat: implement hero-section

---
+++ components/hero.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export function Hero() {
+    return (
+        <View style={styles.heroContainer}>
+            <Text style={styles.heroText1}>
+                Discover fungi with a snap
+            </Text>
+            <Text style={styles.heroText2}>Identify any mushroom instantly with your camera. Build your collection and track where you found each species.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    heroContainer: {
+        backgroundColor: '#873414',
+        padding: 5,
+    },
+    heroText1: {
+        color: '#ffffffff',
+        fontSize: 30
+    },
+    heroText2: {
+        color: '#ffffffff',
+        fontSize: 15
+    },
+});
